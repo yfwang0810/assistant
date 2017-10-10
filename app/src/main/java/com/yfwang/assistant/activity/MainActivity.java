faf1d9f13febc09c7f4e07cc3737f8a28a13a409
王亚飞 <yafei.wang@wwwarehouse.com>
2017-10-10 16:32:22 +0800

    基础框架构建

--- app/src/main/java/com/yfwang/assistant/activity/MainActivity.java
+++ app/src/main/java/com/yfwang/assistant/activity/MainActivity.java
@@ -1,15 +1,56 @@
 package com.yfwang.assistant.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.yfwang.assistant.R;
+import com.yfwang.assistant.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    NavigationView nv;
+    DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
+
+    }
+
+    private void initData() {
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        //设置Toolbar和DrawerLayout实现动画和联动
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    private void initView() {
+        toolbar = (Toolbar) findViewById(R.id.toobar);
+        tabLayout = (TabLayout) findViewById(R.id.tab);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        nv = (NavigationView) findViewById(R.id.nav_view);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 }
